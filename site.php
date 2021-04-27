f9afe2abbd851a5f5e01944d2b8bbbd9f033f7e0
Kirill_kent <kirill.nika1988@gmail.com>
2021-04-27 15:35:40 +0500

    попытка 2

---
+++ site.php
@@ -0,0 +1,18 @@
+<?php 
+    $title = 'Title';
+    $content = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum autem voluptatum animi dicta quasi ab ex optio, odit quam, mollitia itaque distinctio ratione impedit nobis, unde dolores enim? Natus, animi?
+    ';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title?></title>
+</head>
+<body>
+    <h1><?=$content?></h1>
+</body>
+</html>
